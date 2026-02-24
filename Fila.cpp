de02c92e2107ff7bb16a2bb6f130a2e02ca8079b
2830482511008 <2830482511008@fatecsdp.acad.local>
2026-02-24 11:19:51 -0300

    initial commit

---
+++ Fila.cpp
@@ -0,0 +1,54 @@
+/*
+	Name: Fila.cpp
+	Author: Marcos Vinicius
+	Date: 24/02/26 10:57
+	Description: Programa para implementar uma fila do tipo FIFO
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int itens[5];
+int inicio = 0;
+int tam = 5;
+
+//prototype
+void enqueue(int num);
+int dequeue();
+bool isFull();
+bool isEmpty();
+void printQueue();
+
+int main() 
+{
+    printf("shauhsua");
+
+    return 0;
+}
+
+void enqueue(int num)
+{
+	...
+}
+
+
+int dequeue()
+{
+	...
+}
+
+bool isFull()
+{
+	...
+}
+
+bool isEmpty()
+{
+	if (inicio = -1) return true;
+	else return false;
+}
+
+void printQueue()
+{
+	...
+}
