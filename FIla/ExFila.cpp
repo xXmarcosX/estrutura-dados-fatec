673d9418bdd485682e93c126b0e94a092f21d52e
xXmarcosX <marcos.v3567@gmail.com>
2026-03-03 07:50:42 -0300

    switch case

---
+++ FIla/ExFila.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isEmpty();
+bool isFull();
+void enqueue(int num);
+int dequeue();
+
+int inicio = -1, fim = -1;
+int tam = 5;
+int fila[5];
+
+int main()
+{
+
+    return 0;
+}
+
+bool isEmpty() { return inicio == -1; }
+
+bool isFull()
+{
+    return (inicio == (fim + 1) % tam || inicio == 0 && fim == tam - 1);
+}
+
+void enqueue(int num)
+{
+    if (isFull())
+        printf("A FILA ESTA CHEIA");
+    else
+    {
+        if (inicio == -1)
+            inicio - 0;
+
+        fim = (fim + 1) % tam;
+        fila[fim] = num;
+    }
+}
+
+int dequeue()
+{
+    int num = 0;
+
+    if (isEmpty())
+    {
+        return -1;
+    }
+    else
+    {
+    }
+}
--- FIla/Fila.cpp
+++ FIla/Fila.cpp
@@ -12,56 +12,67 @@ int itens[5];
 int inicio = -1, fim = -1;
 int tam = 5;
 
-//prototype
+// prototype
 void enqueue(int num);
 int dequeue();
 bool isFull();
 bool isEmpty();
 void printQueue();
 
-int main() 
+int main()
 {
-    enqueue(100);
-	enqueue(101);
-	enqueue(102);
-	enqueue(103);
-	enqueue(104);
-	
-	puts("");
-	enqueue(154);
-	
-	puts("");
-	enqueue(154);
-	
-	printQueue();
-	
-	dequeue();
-	dequeue();
-	dequeue();
-	
-	printQueue();
-	
-	dequeue();
-	dequeue();
-	dequeue();
-	
-	printQueue();
-	
-	enqueue(114);
-	enqueue(184);
-	enqueue(174);
-	
-	printQueue();
-	
-    return 0;
+	int opt = 0, num = 0;
+	int isRunning = true;
+
+	printf("\n\n1 - enqueue");
+	printf("\n2 - dequeue");
+	printf("\n3 - exibir fila");
+	printf("\n0 - sair");
+
+	while (isRunning)
+	{
+		printf("\n\nDigite a parada: ");
+		scanf("%d", &opt);
+
+		switch (opt)
+		{
+		case 1:
+			printf("\nDigite o numero que sera adicionado a fila: ");
+			scanf("%d", &num);
+
+			enqueue(num);
+			break;
+
+		case 2:
+			dequeue();
+			break;
+
+		case 3:
+			printQueue();
+			break;
+
+		case 0:
+			printf("Saindo...");
+			isRunning = false;
+			break;
+
+		default:
+			printf("\nOpcao invalida");
+			break;
+		}
+	}
+
+	return 0;
 }
 
 void enqueue(int num)
 {
-	if (isFull()) printf("A fila esta cheia"); 
-	else 
+	if (isFull())
+		printf("A fila esta cheia");
+	else
 	{
-		if (inicio == -1) inicio = 0;
+		if (inicio == -1)
+			inicio = 0;
 
 		fim = (fim + 1) % tam;
 		itens[fim] = num;
@@ -69,7 +80,6 @@ void enqueue(int num)
 		printf("\nElemento %d inserido na fila", num);
 	}
 }
-
 
 int dequeue()
 {
@@ -90,11 +100,11 @@ int dequeue()
 			inicio = -1;
 			fim = -1;
 		}
-		else 
+		else
 		{
 			inicio = (inicio + 1) % tam;
 		}
-		
+
 		printf("\n%d Foi excluido da fila", num);
 
 		return num;
@@ -103,21 +113,28 @@ int dequeue()
 
 bool isFull()
 {
-	if (inicio == (fim + 1) % tam || (inicio == 0 && fim == tam - 1)) return true;
-	else return false;
+	if (inicio == (fim + 1) % tam || (inicio == 0 && fim == tam - 1))
+		return true;
+	else
+		return false;
 }
 
 bool isEmpty()
 {
-	if (inicio == -1) return true;
-	else return false;
+	if (inicio == -1)
+		return true;
+	else
+		return false;
 }
 
- void printQueue()
- {
- 	if (isEmpty()) printf("\n\nA FILA ESTA VAZIA");
- 	else {
- 		printf("\n\nConteudo da fila: ");
- 		for (int i = inicio; i <= fim; i++) printf("%d |", itens[i]);	
-	 }
- }
+void printQueue()
+{
+	if (isEmpty())
+		printf("\n\nA FILA ESTA VAZIA");
+	else
+	{
+		printf("\n\nConteudo da fila: ");
+		for (int i = inicio; i <= fim; i++)
+			printf("%d |", itens[i]);
+	}
+}
