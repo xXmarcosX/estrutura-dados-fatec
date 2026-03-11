443a695c33cd310c38f1fc96858bc38a12dac11f
xXmarcosX <marcos.v3567@gmail.com>
2026-03-10 21:22:17 -0300

    Implementação de conversor binário com pilha

--- FIla/ExFila.cpp
+++ FIla/ExFila.cpp
@@ -16,36 +16,22 @@ int main()
     return 0;
 }
 
-bool isEmpty() { return inicio == -1; }
-
-bool isFull()
-{
-    return (inicio == (fim + 1) % tam || inicio == 0 && fim == tam - 1);
+bool isEmpty() {
+    return inicio == -1;
 }
 
-void enqueue(int num)
-{
-    if (isFull())
-        printf("A FILA ESTA CHEIA");
-    else
-    {
-        if (inicio == -1)
-            inicio - 0;
+bool isFull() {
+    return inicio == (fim + 1) % tam || inicio == 0 && fim == tam - 1;
+}
+
+void enqueue(int num) {
+    if (isFull()) printf("A FILA ESTA CHEIA");
+    else {
+        if (inicio == - 1) inicio = 0;
 
         fim = (fim + 1) % tam;
         fila[fim] = num;
-    }
-}
 
-int dequeue()
-{
-    int num = 0;
-
-    if (isEmpty())
-    {
-        return -1;
-    }
-    else
-    {
+        printf("%d foi adicionad a fila", num);
     }
 }
